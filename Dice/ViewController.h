90889bc373e6e72f0228174d757b828ac02c70ef
Chickenpup <christophergshea@gmail.com>
2015-08-01 13:32:24 -0400

    Created project

---
+++ Dice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dice
+//
+//  Created by Chris on 8/1/15.
+//  Copyright (c) 2015 chuppy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
